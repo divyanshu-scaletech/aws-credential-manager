ea9d98450fd677a80dd259765766b1b48a13c859
Divyanshu Motivaras <divyanshu.motivaras@scaletech.xyz>
2024-12-04 12:37:47 +0530

    test cases for auth module

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,18 +1,124 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { AuthRepository } from './auth.repository';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+import {
+  IncorrectPasswordError,
+  UserNotAcceptedError,
+  UserNotFoundError,
+} from './auth.custom-erros';
+import { UnauthorizedException } from '@nestjs/common';
+
+const moduleMocker = new ModuleMocker(global);
+const mockFactory = (token: unknown) => {
+  if (typeof token === 'function') {
+    const mockMetadata = moduleMocker.getMetadata(
+      token,
+    ) as MockFunctionMetadata<any, any>;
+    const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+    return new Mock();
+  }
+};
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-    }).compile();
+      providers: [AuthService, AuthRepository],
+    })
+      .useMocker(mockFactory)
+      .compile();
 
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('Login', () => {
+    it('should be defined', () => {
+      expect(controller.login).toBeDefined();
+    });
+
+    it('should call login function from AuthService', () => {
+      const testUsername = 'test';
+      const testPassword = 'Password@1234';
+
+      jest.spyOn(service, 'login').mockImplementation(
+        () =>
+          new Promise((resolve) => {
+            resolve('some jwt');
+          }),
+      );
+
+      controller.login({
+        username: testUsername,
+        password: testPassword,
+      });
+      expect(service.login).toHaveBeenCalled();
+    });
+
+    it('should map IncorrectPasswordError to UnauthorizedException', () => {
+      const testUsername = 'test';
+      const testPassword = 'Password@1234';
+
+      jest.spyOn(service, 'login').mockImplementation(
+        () =>
+          new Promise((_resolve, reject) => {
+            reject(new IncorrectPasswordError());
+          }),
+      );
+
+      expect(
+        controller.login({
+          username: testUsername,
+          password: testPassword,
+        }),
+      ).rejects.toThrow(UnauthorizedException);
+    });
+
+    it('should map UserNotFoundError to UnauthorizedException', () => {
+      const testUsername = 'test';
+      const testPassword = 'Password@1234';
+
+      jest.spyOn(service, 'login').mockImplementation(
+        () =>
+          new Promise((_resolve, reject) => {
+            reject(new UserNotFoundError());
+          }),
+      );
+
+      expect(
+        controller.login({
+          username: testUsername,
+          password: testPassword,
+        }),
+      ).rejects.toThrow(UnauthorizedException);
+    });
+
+    it('should map UserNotAcceptedError to UnauthorizedException', () => {
+      const testUsername = 'test';
+      const testPassword = 'Password@1234';
+
+      jest.spyOn(service, 'login').mockImplementation(
+        () =>
+          new Promise((_resolve, reject) => {
+            reject(new UserNotAcceptedError());
+          }),
+      );
+
+      expect(
+        controller.login({
+          username: testUsername,
+          password: testPassword,
+        }),
+      ).rejects.toThrow(UnauthorizedException);
+    });
   });
 });
--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -7,7 +10,17 @@ describe('AuthService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     service = module.get<AuthService>(AuthService);
   });
