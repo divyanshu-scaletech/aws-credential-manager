f375a9d065ac87d449c5f4c39d21c9854cc53192
Divyanshu Motivaras <divyanshu.motivaras@scaletech.xyz>
2024-12-05 09:29:26 +0530

    - installed packages for typeorm migrations - generate initial migration

---
+++ src/ormconfig.ts
@@ -0,0 +1,23 @@
+import * as dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+import { entities } from './entities';
+
+dotenv.config();
+
+export const migrationFolder = '**/migrations/**/*.{ts,js}';
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: +process.env.DATABASE_PORT!,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DATABASE,
+  entities: entities,
+  migrations: [migrationFolder],
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
